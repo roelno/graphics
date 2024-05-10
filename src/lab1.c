b303051b706cc17ab2caea70e59ef4a8e2ef5fbd
zelda <lichtseeker@gmail.com>
2024-05-09 19:33:14 -0700

    Make flower more red and leaves more green

--- src/lab1.c
+++ src/lab1.c
@@ -17,6 +17,8 @@ int main(int argc, char *argv[]) {
   long imagesize;
   long i, j;
   int min;
+  int redDecreaseFactor = 50;  // Amount to decrease red color
+  int greenIncreaseFactor = 30; // Amount to increase green color
 
   if(argc < 3) {
     printf("Usage: ppmtest <input file> <output file>\n");
@@ -33,13 +35,19 @@ int main(int argc, char *argv[]) {
   /* calculate the image size */
   imagesize = (long)rows * (long)cols;
 
-  /* turn image to green  */
-  for(i=0; i<imagesize; i++) {
-    image[i].g = 255;
-    image[i].r = 0;
-    image[i].b = 0;
-  }
+  /* mess with the image here  */
+  /* Adjust colors in the image */
+  for(i=0;i<imagesize;i++) {
+    // Decrease redness in specific red range
+    if(image[i].r > 100 && image[i].r < 200) {
+      image[i].r = image[i].r - redDecreaseFactor > 0 ? image[i].r - redDecreaseFactor : 0;
+    }
 
+    // Increase greenness in specific green range
+    if(image[i].g > 50 && image[i].g < 150) { // Assuming leaves are in this green range
+      image[i].g = image[i].g + greenIncreaseFactor < 255 ? image[i].g + greenIncreaseFactor : 255;
+    }
+  }
 
   /* write out the resulting image */
   writePPM(image, rows, cols, colors /* s/b 255 */, argv[2]);
