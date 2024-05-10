2a7288ec5a9cb75273da13b89b17d2b93e12f422
zelda <lichtseeker@gmail.com>
2024-05-09 20:09:30 -0700

    Multiply it by a ramp

--- src/lab1.c
+++ src/lab1.c
@@ -5,9 +5,10 @@
   Bruce A. Maxwell updated 2021-09-05
 */
 
+#include "ppmIO.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "ppmIO.h"
 
 #define USECPP 0
 
@@ -15,19 +16,19 @@ int main(int argc, char *argv[]) {
   Pixel *image;
   int rows, cols, colors;
   long imagesize;
-  long i, j;
-  int min;
-  int redDecreaseFactor = 50;  // Amount to decrease red color
+  long i;
+  int redDecreaseFactor = 50;   // Amount to decrease red color
   int greenIncreaseFactor = 30; // Amount to increase green color
+  float rampFactor;             // Factor to apply the ramp effect
 
-  if(argc < 3) {
+  if (argc < 3) {
     printf("Usage: ppmtest <input file> <output file>\n");
     exit(-1);
   }
 
   /* read in the image */
   image = readPPM(&rows, &cols, &colors, argv[1]);
-  if(!image) {
+  if (!image) {
     fprintf(stderr, "Unable to read %s\n", argv[1]);
     exit(-1);
   }
@@ -37,15 +38,35 @@ int main(int argc, char *argv[]) {
 
   /* mess with the image here  */
   /* Adjust colors in the image */
-  for(i=0;i<imagesize;i++) {
+  for (i = 0; i < imagesize; i++) {
     // Decrease redness in specific red range
-    if(image[i].r > 100 && image[i].r < 200) {
-      image[i].r = image[i].r - redDecreaseFactor > 0 ? image[i].r - redDecreaseFactor : 0;
+    if (image[i].r > 100 && image[i].r < 200) {
+      image[i].r = image[i].r - redDecreaseFactor > 0
+                       ? image[i].r - redDecreaseFactor
+                       : 0;
     }
 
     // Increase greenness in specific green range
-    if(image[i].g > 50 && image[i].g < 150) { // Assuming leaves are in this green range
-      image[i].g = image[i].g + greenIncreaseFactor < 255 ? image[i].g + greenIncreaseFactor : 255;
+    if (image[i].g > 50 &&
+        image[i].g < 150) { // Assuming leaves are in this green range
+      image[i].g = image[i].g + greenIncreaseFactor < 255
+                       ? image[i].g + greenIncreaseFactor
+                       : 255;
+    }
+  }
+
+  /* Apply a horizontal square root ramp */
+  for (int y = 0; y < rows; y++) {
+    for (int x = 0; x < cols; x++) {
+      i = y * cols + x; // Index of the pixel in the linear array
+      rampFactor =
+          sqrt((float)x /
+               (cols - 1)); // Normalized position of pixel using square root
+
+      // Apply the ramp factor to each color channel
+      image[i].r = (int)(image[i].r * rampFactor);
+      image[i].g = (int)(image[i].g * rampFactor);
+      image[i].b = (int)(image[i].b * rampFactor);
     }
   }
 
@@ -59,5 +80,5 @@ int main(int argc, char *argv[]) {
   free(image);
 #endif
 
-  return(0);
+  return (0);
 }
